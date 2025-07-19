4f7f7f8c5cb2839daba4146e1019c95d37ed45e9
The-Lonely-Guy-1798 <thelonelyguy1798@gmail.com>
2025-07-19 15:51:12 +0530

    Update crypto_api_service.dart

--- lib/services/crypto_api_service.dart
+++ lib/services/crypto_api_service.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:fl_chart/fl_chart.dart';
 import '../models.dart'; 
 
 class CryptoApiService {
@@ -33,9 +32,6 @@ class CryptoApiService {
                 name: id.substring(0, 1).toUpperCase() + id.substring(1),
                 price: (data[id]['usd'] as num).toDouble(),
                 change24h: (data[id]['usd_24h_change'] as num).toDouble(),
-
-                // --- FIXED: Initialize with an empty Map {} instead of a List [] ---
-                priceData: {}, 
               ),
             );
           }
@@ -43,43 +39,6 @@ class CryptoApiService {
         return cryptoList;
       } else {
         throw Exception('Failed to load crypto prices');
-      }
-    } catch (e) {
-      throw Exception('Failed to connect to the network: $e');
-    }
-  }
-
-  Future<List<FlSpot>> getHistoricalChartData(String coinId, TimeRange timeRange) async {
-    int days;
-    switch (timeRange) {
-      case TimeRange.oneDay:
-        days = 1;
-        break;
-      case TimeRange.oneWeek:
-        days = 7;
-        break;
-      case TimeRange.oneMonth:
-        days = 30;
-        break;
-      case TimeRange.sixMonths:
-        days = 180;
-        break;
-    }
-
-    final url = Uri.parse('$_baseUrl/coins/$coinId/market_chart?vs_currency=usd&days=$days&interval=daily');
-
-    try {
-      final response = await http.get(url);
-      if (response.statusCode == 200) {
-        final Map<String, dynamic> data = json.decode(response.body);
-        final List<dynamic> prices = data['prices'];
-        
-        return prices.asMap().entries.map((entry) {
-          // The API returns timestamp in milliseconds for X, and price for Y.
-          return FlSpot((entry.value[0] as num).toDouble(), (entry.value[1] as num).toDouble());
-        }).toList();
-      } else {
-        throw Exception('Failed to load chart data for $coinId');
       }
     } catch (e) {
       throw Exception('Failed to connect to the network: $e');
